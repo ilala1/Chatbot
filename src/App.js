e65eb2b15b1de23cbb3267b18631a16002d35009
Imran Lala <imran@enigma-marketing.co.uk>
2021-02-28 17:11:05 +0000

    added theme change functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import { Component } from 'react';
 import './styles/App.scss';
 
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 
 
 import store from './store';
@@ -9,14 +9,50 @@ import store from './store';
 import Chat from './components/chat/Chat'
 
 
-const App = () => {
-  return (
-    <Provider store={store}>
-      <div className="container">
-          <Chat />
-      </div>
-    </Provider>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            darkTheme: true
+        };
+    }
+
+    handleThemeChange = () => {
+        console.log('pre ' + this.state.darkTheme)
+        this.setState(prevState => ({
+            darkTheme: !prevState.darkTheme
+        }));
+        console.log(this.state.darkTheme)
+    }
+
+    render() {
+        return (
+            <Provider store={store}>
+                <div className={`container ${this.state.darkTheme ? 'dark' : 'light'}`}>
+                    <div className='themeIcon' onClick={this.handleThemeChange}>
+                        <p className="themeTitle">Click to change theme</p>
+                        {this.state.darkTheme ? <i class="fas fa-sun"></i> : <i class="fas fa-moon"></i>}
+                        {this.state.darkTheme ? <p>Dark Theme</p> : <p>Light Theme</p>}
+                    </div>
+                    <Chat />
+                </div>
+            </Provider>
+        )
+    }
 }
+
+// const App = () => {
+//   const changeTheme = () => {
+
+//   }
+//   return (
+//     <Provider store={store}>
+//       <div className="container">
+//           <button onClick={changeTheme}>Change theme</button>
+//           <Chat />
+//       </div>
+//     </Provider>
+//   );
+// }
 
 export default App;
