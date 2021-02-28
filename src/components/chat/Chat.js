354fd7b19a4d28e13dbf4ba4aef041f96a80af3c
Imran Lala <imran@enigma-marketing.co.uk>
2021-02-28 12:52:00 +0000

    clean up

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,19 +1,19 @@
-import React, {useState} from 'react'
-import {connect} from 'react-redux';
+import React, { useState } from 'react'
+import { connect } from 'react-redux';
 import uuid from 'react-uuid';
 
-import {sendMessage, userMessage} from '../../actions/jarvis'
+import { sendMessage, userMessage } from '../../actions/jarvis'
 import script from '../../lib/script';
 
 import bot from '../../images/bot.png';
 import user from '../../images/user.png';
-import typing from '../../images/typing.png';
 
 
-const Chat = ({chat, userMessage, sendMessage}) => {
+const Chat = ({ chat, userMessage, sendMessage }) => {
     // HANDLE USERS MESSAGE
     const [message, setMessage] = useState("");
 
+    
     const handleClick = async (e) => {
         const code = e.keyCode || e.which;
         if (code === 13) {
@@ -24,35 +24,31 @@ const Chat = ({chat, userMessage, sendMessage}) => {
         }
     }
     return (
-        <div className='chat'>
+        <div className='chatBoxContainer'>
             <h1 className="title">Welcome to chatbot</h1>
             <div className="historyContainer">
-
                 <div className="chatBubble">
-                <img className="botIcon" src={bot} alt="bot icon" />
+                    <img className="botIcon" src={bot} alt="bot icon" />
                     <div className='bot'>Hi, I'm bot!</div>
 
                 </div>
-
-
-                
-                {chat.length === 0 ? "" : chat.map((msg)=> 
+                {chat.length === 0 ? "" : chat.map((msg) =>
                     <div key={uuid()} className="chatBubble">
-                        {msg.type === 'bot' 
-                        ? 
-                        <img className="botIcon" src={bot} alt="bot icon" /> 
-                        :
-                        msg.type === 'user'
-                        ?
-                        <img className="userIcon" src={user} alt="user icon" />
-                        :
-                        <img className="botIcon" src={bot} alt="bot icon" /> 
+                        {msg.type === 'bot'
+                            ?
+                            <img className="botIcon" src={bot} alt="bot icon" />
+                            :
+                            msg.type === 'user'
+                                ?
+                                <img className="userIcon" src={user} alt="user icon" />
+                                :
+                                <img className="botIcon" src={bot} alt="bot icon" />
                         }
                         <div className={msg.type}>{msg.message}</div>
                     </div>
                 )}
-                <input id="chatBox" 
-                    onChange={(e)=>setMessage(e.target.value)} 
+                <input id="chatBox"
+                    onChange={(e) => setMessage(e.target.value)}
                     onKeyPress={handleClick}
                     value={message}>
                 </input>
@@ -65,4 +61,4 @@ const mapStateToProps = state => ({
     chat: state.jarvis.messages
 })
 
-export default connect(mapStateToProps, {userMessage, sendMessage})(Chat);
+export default connect(mapStateToProps, { userMessage, sendMessage })(Chat);
